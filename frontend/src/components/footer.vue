8e8676e43b905fb7ce85c25141d7cd52568a5775
dlavinia <dlavinia2003@gmail.com>
2022-11-11 23:22:21 -0300

    adjust home and add footer

---
+++ frontend/src/components/footer.vue
@@ -0,0 +1,40 @@
+<template>
+    <footer>
+       <nav>
+            <ul>
+                <li> <a href="https://github.com/dlavinia"><img src="@/assets/github.svg" title="GitHub" alt="GitHub"></a></li>
+                <li> <a href="https://www.linkedin.com/in/d%C3%A9bora-melo-8b03aa205/"><img src="@/assets/linkedin.svg" title="Linkedin" alt="=LinkedIn"></a></li>
+                <li> <a href="emailto:dlavinia2003@gmail.com"><img src="@/assets/gmail.svg" title="Email" alt="Email"></a></li>
+            </ul>
+       </nav>
+    </footer>
+  </template>
+<script>
+
+    export default {
+        name: "FooterApp",
+    }
+</script>
+<style scoped>
+footer{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding:1rem;
+}
+
+ul{
+    display: flex;
+    justify-content: center;
+    list-style: none;
+}
+
+ img{
+width: 50px;
+}
+
+a{
+    padding-right: 16px;
+}
+
+</style>
